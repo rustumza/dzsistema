501924ba5ecf4a9764ab117a2d908ca913029f12
tons <guillepuntons@gmail.com>
2011-04-10 20:25:31 +0000

    sistema

--- dzsistema/src/InterfacesGraficas/ModeloTablaProducto.java
+++ dzsistema/src/InterfacesGraficas/ModeloTablaProducto.java
@@ -8,9 +8,6 @@ package InterfacesGraficas;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
-import Negocio.Entidades.*;
-import javax.swing.DefaultCellEditor;
-import javax.swing.JComboBox;
 /**
  *
  * @author diego
@@ -18,125 +15,103 @@ import javax.swing.JComboBox;
 
 
 public class ModeloTablaProducto extends AbstractTableModel {
-    private static int cantidadDeColumnas = 5;
-    private static int cantidadDeFlias = 15;
+
 
 
 
     //private List<DetalleFactura> listaDetalles;
     public static String[] columnName = {"Cantidad", "Código", "Descripción", "P. Unitario", "Importe"};
-    boolean[] canEdit = new boolean[]{true, true, false, true, false};
-    Object[][] datos = {{new Float(0.0), new Integer(0), " ", new Float(0.0), new Float(0.0)}};
-            
-            
-            //new Object[cantidadDeFlias][cantidadDeColumnas];
+    //boolean[] canEdit = new boolean[]{true, true, false, true, false}; //este arreglo en un principio permitia elegir que campos eran editables
+
+    List<DTODetallesDeFacturaParaGUI> datos;
+
 
 
     public ModeloTablaProducto(){
         super();
 
-
-        /*for (int j = 0; j < cantidadDeFlias; j++) {
-                datos[j][0] = new Float(0.0);
-                datos[j][1] = new Integer(0);
-                datos[j][2] = " ";
-                datos[j][3] = new Float(0.0);
-                datos[j][4] = new Float(0.0);
-        }
-*/
-
-
     }
 
+
+    //Permitir que se editen los campos
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
-        return canEdit[columnIndex];
+        return false; //como simpre devuelve false, no se puede editar ningun campo
     }
 
-    /*public void setCellEditable(boolean edit) {
-        editable = edit;
-    }*/
 
-    /*public void setListaDetalles(List<DetalleFactura> nuevaLista) {
-        if (listaDetalles == null) {
-            listaDetalles = new ArrayList<DetalleFactura>();
+    public void setListaDetalles(List<DTODetallesDeFacturaParaGUI> nuevaLista) {
+        if (datos == null) {
+            datos = new ArrayList<DTODetallesDeFacturaParaGUI>();
         }
-        listaDetalles = nuevaLista;
+        datos = nuevaLista;
         fireTableDataChanged();
     }
-*/
+
     public int getRowCount() {
-        return datos.length;
+        return datos.size();
     }
 
     public int getColumnCount() {
         return columnName.length;
     }
 
-    /*public Object getValueAt(int rowIndex, int columnIndex) {
+    public Object getValueAt(int rowIndex, int columnIndex) {
 
-        if (listaDetalles == null) {
+        if (datos == null) {
             return null;
         }
 
-        DTODetalleInformeReparacion detalle = listaDetalles.get(rowIndex);
+        DTODetallesDeFacturaParaGUI detalle = datos.get(rowIndex);
 
         switch (columnIndex) {
             case 0:
-                return detalle.getFalla();
+                //return detalle.getFalla();
             case 1:
-                return detalle.getEstadoFalla();
+                //return detalle.getEstadoFalla();
             case 2:
-                return detalle.getComentario();
+                //return detalle.getComentario();
             default:
                 return "";
         }
-    }*/
+    }
 
     @Override
     public String getColumnName(int column) {
         return columnName[column];
     }
 
-/*    public void addRow(DetalleFactura nuevoDetalle) {
-        if (listaDetalles == null) {
-            listaDetalles = new ArrayList<DetalleFactura>();
+    public void addRow(DTODetallesDeFacturaParaGUI DTODetalle) {
+        if (datos == null) {
+            datos = new ArrayList<DTODetallesDeFacturaParaGUI>();
         }
-        listaDetalles.add(nuevoDetalle);
+        datos.add(DTODetalle);
         fireTableDataChanged();
     }
 
-    public void addAllRow(List<DetalleFactura> nuevaLista) {
-        if (listaDetalles == null) {
-            listaDetalles = new ArrayList<DetalleFactura>();
+    public void addAllRow(List<DTODetallesDeFacturaParaGUI> nuevaLista) {
+        if (datos == null) {
+            datos = new ArrayList<DTODetallesDeFacturaParaGUI>();
         }
-        listaDetalles.addAll(nuevaLista);
+        datos.addAll(nuevaLista);
         fireTableDataChanged();
     }
 
     public Object getRow(int rowIndex) {
-        return listaDetalles.get(rowIndex);
+        return datos.get(rowIndex);
     }
 
     public void clear() {
-        if (listaDetalles == null) {
+        if (datos == null) {
             return;
         }
-        listaDetalles.clear();
+        datos.clear();
         fireTableDataChanged();
     }
 
     public void removeRow(int rowIndex) {
-        listaDetalles.remove(rowIndex);
+        datos.remove(rowIndex);
         fireTableDataChanged();
     }
-*/
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        return datos[rowIndex][columnIndex];
-    }
 
-    public void agregarOtroItemBlanco(Object[] datos){
-
-
-    }
 }
