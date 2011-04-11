f76fed9ae51cd32a2254bb503983201725a97394
tons <guillepuntons@gmail.com>
2011-04-11 23:29:00 +0000

    sistema

--- dzsistema/src/InterfacesGraficas/DTODetallesDeFacturaParaGUI.java
+++ dzsistema/src/InterfacesGraficas/DTODetallesDeFacturaParaGUI.java
@@ -10,5 +10,65 @@ package InterfacesGraficas;
  * @author rustu
  */
 public class DTODetallesDeFacturaParaGUI {
+    private float cantidad;
+    private String codigo;
+    private float precioUnitario;
+    private float importe;
+
+    /**
+     * @return the cantidad
+     */
+    public float getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(float cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the precioUnitario
+     */
+    public float getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    /**
+     * @param precioUnitario the precioUnitario to set
+     */
+    public void setPrecioUnitario(float precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    /**
+     * @return the importe
+     */
+    public float getImporte() {
+        return importe;
+    }
+
+    /**
+     * @param importe the importe to set
+     */
+    public void setImporte(float importe) {
+        this.importe = importe;
+    }
 
 }
--- dzsistema/src/Negocio/Facturacion/ExpertoFacturar.java
+++ dzsistema/src/Negocio/Facturacion/ExpertoFacturar.java
@@ -5,8 +5,10 @@
 
 package Negocio.Facturacion;
 
+import InterfacesGraficas.DTODetallesDeFacturaParaGUI;
 import Negocio.Entidades.Cliente;
 import Negocio.Entidades.ClienteJpaController;
+import Negocio.Entidades.DetalleFactura;
 import Negocio.Entidades.Factura;
 import Negocio.Entidades.Producto;
 import java.util.Date;
@@ -76,10 +78,39 @@ public class ExpertoFacturar {
         //TO DO
     }
 
-    public Producto buscarProducto(String text) {
+    public Producto buscarProducto(String nombre, String fecha) {
+        //SETEAR ESA FECHA A LA FACTURA
         //busco producto con la fecha de la factura
         return null;
         //TO DO
+    }
+
+    private Producto buscarProducto(String nombre){
+        //esto es para buscar un producto que no importe la fecha del precio historico
+        //TO DO
+        return null;
+    }
+
+    public Factura AgregarDetalleALaFactura(DTODetallesDeFacturaParaGUI dto){
+        DetalleFactura detalleFactura = new DetalleFactura();
+        detalleFactura.setProducto(buscarProducto(dto.getCodigo()));
+        detalleFactura.setPorcentajeDeIva(detalleFactura.getProducto().getPorcentajeDeIva());
+        detalleFactura.setCantidad(dto.getCantidad());
+        detalleFactura.setPrecioUnitario(dto.getPrecioUnitario());
+        float importe = detalleFactura.getCantidad() * detalleFactura.getPrecioUnitario();
+        detalleFactura.setPrecioTotal(Math.round(importe * 100)/100);
+        if(detalleFactura.getPrecioTotal() != dto.getImporte()){
+            //VER COMO MANEJAR ESTE ERROR //TO DO
+        }
+        //AGREGAR DETALLE A LA FACTURA //TO DO
+        return factura;
+    }
+
+    public boolean comparaFechaFactura(String text) {
+        factura.getFecha();
+        boolean sonIguales = true;
+        //COMPARAR FECHAS,  // TODO
+        return sonIguales;
     }
 
 
