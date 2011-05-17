9bd3f95396ea91b65bc7497772c71c5b4f2ed0d1
tons <guillepuntons@gmail.com>
2011-05-17 14:58:09 +0000

    sistema

--- dzsistema/src/InterfacesGraficas/Main.java
+++ dzsistema/src/InterfacesGraficas/Main.java
@@ -5,6 +5,8 @@
 
 package InterfacesGraficas;
 
+import Negocio.Entidades.Cliente;
+import Negocio.Entidades.ClienteJpaController;
 import Negocio.Entidades.CondicionDeVenta;
 import Negocio.Entidades.CondicionDeVentaJpaController;
 import Negocio.Entidades.CondicionFrenteAlIva;
@@ -108,7 +110,9 @@ public class Main {
 
             FacturaJpaController jpa = new FacturaJpaController();
             Date date1 = new Date((2011-1900),(10-1), 20);
-            Date date2 = new Date((2012-1900),(12-1), 11);
+            Date date2 = new Date((2014-1900),(11-1), 11);
+            ClienteJpaController jpaCli = new ClienteJpaController();
+            Cliente cli = jpaCli.buscarPorCodigo("3").get(0);
             /*List<Factura> lista = jpa.buscarFacturaEntreFechas1(date1, date2);
             System.out.println("---------------------------------------------------------");
             System.out.println("---------------------------------------------------------");
@@ -119,8 +123,8 @@ public class Main {
             for (Factura factura : lista) {
                 System.out.println(factura.getNumero());
             }
-             */
-            /*List<Factura> lista2 = jpa.buscarFacturaEntreFechasSinLaFechaFin(date1, date2);
+            */
+            List<Factura> lista2 = jpa.buscarFacturaEntreFechasSinLaFechaFinYConCliente(date1, date2, cli);
             System.out.println("---------------------------------------------------------");
             System.out.println("---------------------------------------------------------");
             System.out.println("---------------------------------------------------------");
@@ -130,7 +134,8 @@ public class Main {
             for (Factura factura : lista2) {
                 System.out.println(factura.getNumero());
             }
-            */
+            
+            
             // Setea como se ven las pantallas y arranca el sistema
           new ControladorPanallaFacturacion().iniciarPantalla();
         }
--- dzsistema/src/InterfacesGraficas/Reportes/ControladorEvolucionComprasClienteRespectoAUnProducto.java
+++ dzsistema/src/InterfacesGraficas/Reportes/ControladorEvolucionComprasClienteRespectoAUnProducto.java
@@ -5,9 +5,12 @@
 
 package InterfacesGraficas.Reportes;
 
+import Negocio.Reportes.DtoMesCantidadProducto;
 import Negocio.Reportes.ExpertoEvolucionComprasClienteRespectoAUnProducto;
 import java.util.Date;
+import java.util.List;
 import javax.swing.JOptionPane;
+import validar.fechaException;
 
 /**
  *
@@ -27,14 +30,112 @@ public class ControladorEvolucionComprasClienteRespectoAUnProducto {
     }
 
     public void buscarProducto() {
-        
+        int anioInicioInt = 0;
+        int anioFinInt = 0;
+        int mesInicio = 0;
+        int mesFin = 0;
+        int codigoInt = 0;
+
         try{
             String codigo = pantalla.getCodigoProducto().getText();
-            int codigoInt = Integer.parseInt(codigo);
-            experto.buscarHitorialProducto(codigoInt);
+            codigoInt = Integer.parseInt(codigo);
         }catch(NumberFormatException e){
             JOptionPane.showMessageDialog(null, "El código de producto ingresado no es válido", "¡Atención!", JOptionPane.INFORMATION_MESSAGE);
             pantalla.getCodigoProducto().requestFocus();
+            return;
+        }
+        try{
+            String anioInicio = pantalla.getAnioInicio().getText();
+            if(anioInicio.length() != 2){
+                JOptionPane.showMessageDialog(null, "El año de inicio ingresado no es válido", "¡Atención!", JOptionPane.INFORMATION_MESSAGE);
+                pantalla.getAnioInicio().requestFocus();
+            }
+            anioInicioInt = Integer.parseInt(anioInicio);
+
+            String mesInicioString = (String)pantalla.getMesInicioComboBox().getSelectedItem();
+            mesInicio = 0;
+            if(mesInicioString.equals("Enero")){
+                mesInicio = 0;
+            }else if(mesInicioString.equals("Febrero")){
+                mesInicio = 1;
+            }else if(mesInicioString.equals("Marzo")){
+                mesInicio = 2;
+            }else if(mesInicioString.equals("Abril")){
+                mesInicio = 3;
+            }else if(mesInicioString.equals("Mayo")){
+                mesInicio = 4;
+            }else if(mesInicioString.equals("Junio")){
+                mesInicio = 5;
+            }else if(mesInicioString.equals("Julio")){
+                mesInicio = 6;
+            }else if(mesInicioString.equals("Agosto")){
+                mesInicio = 7;
+            }else if(mesInicioString.equals("Setiembre")){
+                mesInicio = 8;
+            }else if(mesInicioString.equals("Octubre")){
+                mesInicio = 9;
+            }else if(mesInicioString.equals("Noviembre")){
+                mesInicio = 10;
+            }else if(mesInicioString.equals("Diciembre")){
+                mesInicio = 11;
+            }
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "El año de inicio ingresado no es válido", "¡Atención!", JOptionPane.INFORMATION_MESSAGE);
+            pantalla.getCodigoProducto().requestFocus();
+            return;
+        }
+
+        try{
+            String anioFin = pantalla.getAnioFin().getText();
+            anioFinInt = Integer.parseInt(anioFin);
+            if(anioFin.length() != 2){
+                JOptionPane.showMessageDialog(null, "El año de fin ingresado no es válido", "¡Atención!", JOptionPane.INFORMATION_MESSAGE);
+                pantalla.getAnioFin().requestFocus();
+            }
+
+            String mesFinString = (String)pantalla.getMesFinComboBox().getSelectedItem();
+            mesFin = 0;
+            if(mesFinString.equals("Enero")){
+                mesFin = 0;
+            }else if(mesFinString.equals("Febrero")){
+                mesFin = 1;
+            }else if(mesFinString.equals("Marzo")){
+                mesFin = 2;
+            }else if(mesFinString.equals("Abril")){
+                mesFin = 3;
+            }else if(mesFinString.equals("Mayo")){
+                mesFin = 4;
+            }else if(mesFinString.equals("Junio")){
+                mesFin = 5;
+            }else if(mesFinString.equals("Julio")){
+                mesFin = 6;
+            }else if(mesFinString.equals("Agosto")){
+                mesFin = 7;
+            }else if(mesFinString.equals("Setiembre")){
+                mesFin = 8;
+            }else if(mesFinString.equals("Octubre")){
+                mesFin = 9;
+            }else if(mesFinString.equals("Noviembre")){
+                mesFin = 10;
+            }else if(mesFinString.equals("Diciembre")){
+                mesFin = 11;
+            }
+
+
+       
+       }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "El año de inicio ingresado no es válido", "¡Atención!", JOptionPane.INFORMATION_MESSAGE);
+            pantalla.getCodigoProducto().requestFocus();
+            return;
+       }
+       try{
+            Date fechaInicio = new Date((anioInicioInt-1900),mesInicio,1);
+            Date fechaFin = new Date((anioFinInt-1900),mesFin,1);
+            List<DtoMesCantidadProducto> listaDtoResultante = experto.buscarHitorialProducto(codigoInt, fechaInicio, fechaFin);
+       }catch(fechaException e){
+            JOptionPane.showMessageDialog(null, e.getMessage(), "¡Atención!", JOptionPane.INFORMATION_MESSAGE);
+            pantalla.getAnioInicio().requestFocus();
+
         }
     }
 }
