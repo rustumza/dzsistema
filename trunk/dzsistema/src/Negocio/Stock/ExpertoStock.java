0790dedfe714928aa256416aadf54cd3148fedb0
guillepuntons@gmail.com <guillepuntons@gmail.com@17d57842-08b0-43cf-5522-405e8e3f1b4f>
2011-10-19 19:32:07 +0000

    sistema

---
+++ trunk/dzsistema/src/Negocio/Stock/ExpertoStock.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Negocio.Stock;
+
+import Negocio.Entidades.Producto;
+
+/**
+ *
+ * @author rustu
+ */
+public class ExpertoStock {
+
+    Producto producto;
+
+
+
+    public int agregarStock(int catidad){
+        //retorna el nuevo stock
+
+        
+
+        return 0;
+    }
+
+    public int restarStock(int cantidad){
+        //retorna el nuevo stock
+
+        return 0;
+    }
+}
